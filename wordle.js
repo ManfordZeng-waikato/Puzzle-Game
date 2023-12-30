3b3d385a96202aaf5bd0615fc34339db5c8bf694
Manford Zeng(cz174)-waikato <zengchang389@gmail.com>
2023-12-30 21:44:08 +1300

    Task Four: Current Guess

--- wordle.js
+++ wordle.js
@@ -13,51 +13,50 @@ buttons.forEach(button => {
     });
 });
 
-// 添加物理键盘事件监听器
-document.addEventListener('keydown', function (event) {
-    const key = event.key.toUpperCase();
 
-    if ((key >= 'A' && key <= 'Z') || key === 'ENTER' || key === 'BACKSPACE' || key === 'ESCAPE') {
-        handleKey(key);
 
-        // 添加可视化指示
-        const matchingButton = Array.from(buttons).find(button => button.innerText === key);
-        if (matchingButton) {
-            matchingButton.classList.add('active');
-            setTimeout(() => {
-                matchingButton.classList.remove('active');
-            }, 200);
-        }
-    }
-});
+
+let currentGuess = [];
+
 
 function handleKey(key) {
     // 处理按键逻辑
     console.log(`Key pressed: ${key}`);
 
     // 处理特殊按键逻辑
-    if (key === 'BACKSPACE') {
+    if (key === 'Backspace') {
+        handleBackspace();
         console.log("Backspace pressed");
     } else if (key === 'ENTER') {
+
+
         console.log("Enter pressed");
     } else if (key === 'ESCAPE') {
         console.log("New Game pressed");
+    } else if (currentGuess.length < 6 && key !== 'Backspace' && key !== 'Enter' && key !== 'New Game') {
+        // 添加字符到当前猜测
+        currentGuess.push(key);
+        console.log(currentGuess);
     }
+    updateCurrentGuess();
 }
 
-const enterButton = document.querySelector('#row4 button:nth-child(2)');
 
-// 添加按钮点击事件监听器
-buttons.forEach(button => {
-    button.addEventListener('click', function () {
-        handleKey(button.innerText);
-        // 添加可视化指示
-        button.classList.add('active');
-        setTimeout(() => {
-            button.classList.remove('active');
-        }, 200);
-    });
-});
+function handleBackspace() {
+    // 处理退格键逻辑
+    if (currentGuess.length > 0) {
+        // 移除最后一个字符
+        currentGuess.pop();
+    }
+
+    // 更新显示的当前猜测
+    updateCurrentGuess();
+}
+
+
+
+
+
 
 // 添加物理键盘事件监听器
 document.addEventListener('keydown', function (event) {
@@ -81,56 +80,20 @@ document.addEventListener('keydown', function (event) {
 let targetWord = "";
 
 // 更新Enter按钮状态
-updateEnterButton();
+
 
 // 添加Enter按钮点击事件监听器
 enterButton.addEventListener('click', function () {
     if (targetWord !== "") {
-        handleKey('ENTER');
+        handleKey('Enter');
     }
 });
 
-// 获取目标单词
-async function getTargetWord() {
-    const response = await fetch('https://words.trex-sandwich.com/api/words', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        // 设置查询参数，length指定单词长度为6
-        // 如果请求失败，使用默认单词"TARGET"
-        // 注意：在实际应用中，需要处理网络请求失败的情况
-        // 另外，这个API可能会有使用限制，请查看文档了解详情
-        // 在实际应用中，可能需要注册API密钥
-        // https://words.trex-sandwich.com/api/docs
-        // 这里我们仅用于演示目的，不考虑API密钥和错误处理
-        url: `https://words.trex-sandwich.com/api/words?count=1&length=6`,
-    });
-
-    if (response.ok) {
-        const data = await response.json();
-        targetWord = data[0];
-        console.log(`Target word: ${targetWord}`);
-        // 更新Enter按钮状态
-        updateEnterButton();
-    } else {
-        console.log('Failed to fetch target word. Using default word "TARGET".');
-        targetWord = "TARGET";
-    }
+function updateCurrentGuess() {
+    let rowIndex = 1;
+    const currentGuessDisplay = document.getElementById("guess-row" + rowIndex);
+    const charList = currentGuessDisplay.querySelectorAll(".guess-char");
+    charList.forEach((charEle, index) => {
+        charEle.innerText = currentGuess[index] || "";
+    })
 }
-
-// 更新Enter按钮状态
-function updateEnterButton() {
-    if (targetWord === "") {
-        // 如果目标单词未选择，禁用Enter按钮
-        enterButton.disabled = true;
-        enterButton.classList.add('disabled');
-    } else {
-        // 目标单词选择后启用Enter按钮
-        enterButton.disabled = false;
-        enterButton.classList.remove('disabled');
-    }
-}
-
-// 初始化目标单词
-getTargetWord();
